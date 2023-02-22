9676350b0b8b94b59ac8788e47f6f6d4a8dea665
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2023-02-22 22:46:28 +0100

    Fix install script

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-sed -i "s/@@HOME_DIR@@/$HOME/g" color-scheme-sync.service
+set -e
+
+sed -i "s#@@HOME_DIR@@#$HOME#g" color-scheme-sync.service
 
 cp target/release/color-scheme-sync ~/.local/bin/color-scheme-sync
 cp color-scheme-sync.service ~/.config/systemd/user/
