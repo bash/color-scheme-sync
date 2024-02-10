eb4a57b42c47941cd6918438b12914f80e5aac17
Tau <git@tau.garden>
2024-02-10 22:23:13 +0100

    Update legacy-gtk-theme.sh

--- scripts/legacy-gtk-theme.sh
+++ scripts/legacy-gtk-theme.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-if [ "$COLOR_SCHEME" = "prefer-light" ]; then
-    gtk_theme=Adwaita
-else
+if [ "$COLOR_SCHEME" = "prefer-dark" ]; then
     gtk_theme=Adwaita-dark
+else
+    gtk_theme=Adwaita
 fi
 
 gsettings set org.gnome.desktop.interface gtk-theme "$gtk_theme"
